485ad14532afebe756238c8574727191d59272f3
Ranjith Ramkishore <ranjith@D-10-19-109-20.dhcp4.washington.edu>
2018-05-09 08:32:48 -0700

    Final Changes

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -3,14 +3,14 @@
 # Define a function `add_three` that takes a single argument and
 # returns a value 3 greater than the input
 add_three <- function(a) {
-  a+3
+  a + 3
 }
-# Create a variable `ten` that is the result of passing 7 to your `add_three` 
+# Create a variable `ten` that is the result of passing 7 to your `add_three`
 # function
 
 ten <- add_three(7)
 
-# Define a function `imperial_to_metric` that takes in two arguments: a number 
+# Define a function `imperial_to_metric` that takes in two arguments: a number
 # of feet and a number of inches
 # The function should return the equivalent length in meters
 
